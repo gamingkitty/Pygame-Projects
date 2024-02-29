1e5fceb6ee669acf1f1ad30ba241c2238e17220d
Brier Hehmeyer <bmhehmeyer@gmail.com>
2024-02-28 20:48:59 -0800

    Fixed shop hud 2

--- games/ghostGame/shopitem.py
+++ games/ghostGame/shopitem.py
@@ -2,8 +2,8 @@ import pygame
 import button
 
 
-def draw_text(text, color, size, x, y, SCREEN, screen_scaler, aligned="center"):
-    font = pygame.font.Font(None, int(size * screen_scaler))
+def draw_text(text, color, size, x, y, SCREEN, aligned="center"):
+    font = pygame.font.Font(None, size)
     text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect()
     if aligned == "center":
@@ -42,9 +42,9 @@ class ShopItem():
 
     def load(self, SCREEN):
         self.button.load(SCREEN)
-        draw_text(str(self.bought), self.text_color, self.font_size, self.location[0] + 40 + self.size[0]/2, self.location[1], SCREEN, self.screen_scaler)
+        draw_text(str(self.bought), self.text_color, self.font_size, self.location[0] + 40 + self.size[0]/2, self.location[1], SCREEN)
         if not self.maxed:
-            draw_text("(" + str(self.price) + ")", self.text_color, self.font_size, self.location[0] - 30 - self.size[0]/2, self.location[1], SCREEN, self.screen_scaler)
+            draw_text("(" + str(self.price) + ")", self.text_color, self.font_size, self.location[0] - 30 - self.size[0]/2, self.location[1], SCREEN)
 
     def buy(self, player):
         if self.upgrade_type == "hp":
